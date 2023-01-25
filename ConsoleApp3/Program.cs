4a61ddd15a3f78573b170632152483d82a984d1e
Admin <Admin@TEB123001>
2023-01-25 11:37:28 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Brogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = { "0. Kina", "1. Indien", "2. USA", "3. Indonesien", "4. Pakistan" };
+            for (int i = 0; i < countries.Length; i++)
+            {
+                Console.WriteLine(countries[i]);
+            }
+            string[] sträng = Console.ReadLine().Split(" ");
+            Int64[] nummer = Array.ConvertAll(sträng, s => Int64.Parse(s)); // Använder Int64 så att man kan multiplicera större tal
+
+            for (int x = 0; x < nummer.Length; x++)
+            {
+                Console.WriteLine(countries[nummer[x]]);
+            }
+
+
+        }
+    }
+}
